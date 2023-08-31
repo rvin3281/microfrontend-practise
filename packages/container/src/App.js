1beae36092fb964da71664bf905c31215f206ffc
arvend <arvendrajan100@gmail.com>
2023-08-31 11:41:58 +0800

    updated container.yml

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>TEst</h1>
+      <h1>Connected to AWS</h1>
       <hr />
       <MarketingApp />
     </div>
